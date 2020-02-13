47cbd72887ea85c18242120b19f7b001477a4d54
KenetGaona115 <aldrinkenet@hotmail.com>
2020-02-13 16:19:16 -0600

    cambios al 13/02/202

--- src/lenuajes_practica_afd/Lenuajes_Practica_AFD.java
+++ src/lenuajes_practica_afd/Lenuajes_Practica_AFD.java
@@ -16,22 +16,22 @@ import java.io.IOException;
  * @author Kenet Gaona
  */
 public class Lenuajes_Practica_AFD {
-    
-        static String entrada = "";
-        static String alfabeto = "";
-        static String q0 = "";
-        static String e_finales = "";
-        static String [][] matriz_F;
+
+    static String entrada = "";
+    static String alfabeto = "";
+    static String q0 = "";
+    static String e_finales = "";
+    static String[][] matriz_F;
+
     /**
      * @param args the command line arguments
      */
-        
     public static void main(String[] args) throws FileNotFoundException, IOException {
         File archivo = null;
         FileReader fr = null;
         BufferedReader br = null;
         String lectura = "";
-        
+
         // Apertura del fichero y creacion de BufferedReader para poder
         // hacer una lectura comoda (disponer del metodo readLine()).
         archivo = new File("D:\\Escritorio\\Lenguajes\\Practica 1\\texto.txt");
@@ -46,50 +46,56 @@ public class Lenuajes_Practica_AFD {
         }
         //System.out.println(lectura);
         getData(lectura);
-        
+
     }
-    
+
     //Funcion para retornara las variables globales
     // los datos que se requieren
-    private static void getData(String x){
-        
+    private static void getData(String x) {
+
         //Entrada
         x = x.substring(9, x.length());
         int i = x.indexOf(";");
-        entrada = x.substring(0, i-1);
+        entrada = x.substring(0, i - 1);
         System.out.println("Entrada: " + entrada);
-        
+
         //Alfabeto
-        x = x.substring(i-1);
+        x = x.substring(i - 1);
         i = x.indexOf(" ");
         alfabeto = x.substring(0, i);
         System.out.println("Alfabeto: " + alfabeto);
-        
+
         //Estado inicial
         x = x.substring(i);
         q0 = x.substring(0, 2);
         System.out.println("Estado inicial: " + q0);
-        
+
         //Estado(s) final
         i = x.indexOf(" ");
-        x = x.substring(i+2);//aunmentamos 1 por la diferencia del espacio
+        x = x.substring(i + 2);//aunmentamos 1 por la diferencia del espacio
         i = x.indexOf(" ");
-        e_finales = x.substring(0,i);
+        e_finales = x.substring(0, i);
         System.out.println("Estados finales: " + e_finales);
-        
+
         //Matriz de transisicones 
         x = x.substring(i);
         int renglones = 0;
         String[] matriz = x.split(" ");
         for (String matriz1 : matriz) {
             System.out.println(matriz1);
-            renglones ++;
+            renglones++;
         }
-        System.out.println("Renglones: "+ renglones);
-       
-        
-        
+        System.out.println("Renglones: " + renglones);
+
+        for (int j = 0; j < renglones; j++) {
+            if (!matriz[i].equals(";")) {
+                for (int k = j; k < j + 1; k++) {
+                        matriz_F[j][k] = matriz[j];
+                        System.out.println(matriz_F[j][k]);
+                }
+            }
+        }
+
     }
-    
 
 }
