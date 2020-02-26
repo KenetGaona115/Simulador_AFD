e6c7adc8ad62b1f60697cb5ea111c5daea1ee31b
KenetGaona115 <aldrinkenet@hotmail.com>
2020-02-26 14:43:20 -0600

    Listo al 26/02/2020

--- src/lenuajes_practica_afd/Lenuajes_Practica_AFD.java
+++ src/lenuajes_practica_afd/Lenuajes_Practica_AFD.java
@@ -5,12 +5,12 @@
  */
 package lenuajes_practica_afd;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,108 +19,104 @@ import java.util.List;
  */
 public class Lenuajes_Practica_AFD {
 
-    static String entrada = "";
-    static String alfabeto = "";
-    static String q0 = "";
-    static String e_finales = "";
     static List<String> Lista;
+    static String[][] transMatrix;
 
     /**
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        File archivo = null;
-        FileReader fr = null;
-        BufferedReader br = null;
-        String lectura = "";
+        //Creamos un archivo para hacer una lectura
+        Path archivo_lectura;
+        archivo_lectura = Paths.get("D:\\Escritorio\\Lenguajes\\lol.txt");
 
-        // Apertura del fichero y creacion de BufferedReader para poder
-        // hacer una lectura comoda (disponer del metodo readLine()).
-        archivo = new File("D:\\Escritorio\\Lenguajes\\texto2.txt");
-        fr = new FileReader(archivo);
-        br = new BufferedReader(fr);
-
-        // Lectura del fichero
-        String linea;
-        while ((linea = br.readLine()) != null) {
-            System.out.println(linea);
-            lectura += linea;
-        }
-        //System.out.println(lectura);
-        getData(lectura);
+        //Leemos el archivo y lo asignamos el texto a una lista
+        List<String> lectura = Files.readAllLines(archivo_lectura);
+        transMatrix = new String[lectura.size()][];
+        
+        //Preguntamos si es que el archivo esta vacio 
+        if(!lectura.isEmpty())
+            getData(lectura);
+        else
+            System.out.println("No hay nada en el archivo");
     }
 
+    
     //Funcion para retornara las variables globales
     // los datos que se requieren
-    private static void getData(String x) {
+    private static void getData(List<String> lines) {
+        String entrada = null;
+        List<String> alfabeto = null;
+        String i_State = null;
+        String[] f_States = null;
 
-        //Entrada
-        x = x.substring(9, x.length());
-        int i = x.indexOf(";");
-        entrada = x.substring(0, i - 1);
-        System.out.println("Entrada: " + entrada);
-
-        //Alfabeto
-        x = x.substring(i - 1);
-        i = x.indexOf(" ");
-        alfabeto = x.substring(0, i);
-        System.out.println("Alfabeto: " + alfabeto);
-
-        //Estado inicial
-        x = x.substring(i);
-        q0 = x.substring(0, 2);
-        System.out.println("Estado inicial: " + q0);
-
-        //Estado(s) final
-        i = x.indexOf(" ");
-        x = x.substring(i + 2);//aunmentamos 1 por la diferencia del espacio
-        i = x.indexOf(" ");
-        e_finales = x.substring(0, i);
-        System.out.println("Estados finales: " + e_finales);
-
-        //Lista de transisicones 
-        x = x.substring(i).replaceAll(";", "").replaceAll(" ", "");
-        System.out.println(x);
-        Lista = toList(x);
-        System.out.println("Estados finales:");
-        
-        //Corroboramos que se quede en un estado final
-        //para garantizar que fue acepatada por el Alfabeto
-        System.out.println(isFinalState(Lista, entrada, e_finales));
-    }
-
-    private static List<String> toList(String x) {
-        List<String> Lista = new ArrayList<>();
-
-        for (int i = 0; i < x.length(); i += 2) {
-            Lista.add(x.substring(i, i + 2));
-        }
-
-        for (int i = 0; i < Lista.size(); i++) {
-            System.out.println(Lista.get(i));
-        }
-        return Lista;
-    }
-
-    private static boolean isFinalState(List<String> y, String x, String f) {
-        String state = "";
-        for (int i = 0; i < x.length(); i++) {
-            state = returnState(y,x.substring(i, i+1));
-            System.out.print(state + "/");
-        }
-        System.out.println("");
-        return f.contains(state);
-    }
-    
-    public static String returnState(List<String> y,String x){
-        //Ciclo para realizar la busqueda del siguiente estado 
-        for (int i = 0; i < y.size(); i++) {
-            if (y.get(i).startsWith(x)) {
-                return y.get(i).substring(+1);
+        for (int i = 0; i < lines.size(); i++) {
+            //se obtiene la entrada a analizar.
+            if (i == 0) {
+                //Utilizamos trim para poder quitar los espacios
+                entrada = lines.get(i).trim();
+            } //se obtiene el alfabeto
+            else if (i == 1) {
+                //Realizamos un split para eliminar el ;
+                //Usamos un asList para poder pasar los parametros
+                alfabeto = Arrays.asList(lines.get(i).trim().split(";"));
+            } //estado inicial.
+            else if (i == 2) {
+                i_State = lines.get(i).trim();
+            } //estados finales.
+            else if (i == 3) {
+                f_States = lines.get(i).trim().split(";");
+            } //obtenemos la matriz de transiciones.
+            else {
+                //Llenamos la matriz con los datos otenidos 
+                transMatrix[i - 4] = lines.get(i).trim().split(";");
             }
         }
-        return null;
+        toStateList(i_State, transMatrix, f_States, entrada, alfabeto);
+    }
+
+    public static void toStateList(String i_State, String[][] transMatrix, String[] f_States, String entrada, List<String> alfabeto) {
+        String r_estados = i_State + "/";
+        int currentState = Integer.parseInt(i_State);
+
+        //Iteramos la cadena de entrada para obtener la secuencia de estados visitados.
+        for (int i = 0; i < entrada.length(); i++) {
+            String symbol = entrada.substring(i, i + 1);
+            currentState = Integer.parseInt(transMatrix[currentState][alfabeto.indexOf(symbol)]);
+            r_estados += currentState + "/";
+        }
+
+        //Consultamos si el último estado visitado es parte de los estados finales. Si sí, imprimimos aceptada. Si no, imprimimos no aceptada.
+        String last = Integer.toString(currentState);
+        boolean x = false;
+
+        for (int i = 0; i < f_States.length; i++) {
+            if (f_States[i].contentEquals(last)) {
+                x = true;
+                break;
+            }
+        }
+
+        //Comparación de si la bandera fue verdadera o falsa.
+        if (x == true) 
+            System.out.println("Aceptada.");
+        else 
+            System.out.println("No Aceptada.");
+        
+        //Impresión de la secuencia de estados.
+        System.out.println("Secuencia de estados: " + r_estados);
+    }
+    
+    //Imprecon de arreglo
+    public static void toStingArray(String[][] transMatrix){
+        for (int i = 0; i < 3; i++) {
+            System.out.print(transMatrix[i][0].toString());
+            for (int j = 0; j < 2; j++) {
+                System.out.print(transMatrix[i][j].toString());
+            }
+            System.out.println("");
+        }
     }
 
 }
